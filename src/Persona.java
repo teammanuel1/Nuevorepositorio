fa87413dbfcf612f9cced694a321c9f4fe820465
Manuel <manuel.rios.herrera.dam@gmail.com>
2019-05-22 14:40:44 +0200

    Primera modificacion clase Persona

---
+++ src/Persona.java
@@ -0,0 +1,15 @@
+
+public class Persona {
+
+	public Persona() {
+		// TODO Apéndice de constructor generado automáticamente
+	}
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+	/*comentario*/
+}
+
+
