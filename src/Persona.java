888a9b3ebf3e378e45e8f5f9dd255b64a2f539a1
Manuel <manuel.rios.herrera.dam@gmail.com>
2019-05-24 13:15:01 +0200

    Segunda modificacion

--- src/Persona.java
+++ src/Persona.java
@@ -1,11 +1,37 @@
 
 public class Persona {
+	int edad;
+	double altura;
+	String nombre;
+	
+	public Persona(int edad, String nombre, double altura) {
+		this.edad = edad;
+		this.altura = altura;
+		this.nombre = nombre;
+	}
 
-	public Persona() {
-		int edad;
-		double altura;
-		String nombre;
-		
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public static void main(String[] args) {
